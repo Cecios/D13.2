9f69b3d5ec28550d698e0cd525671697e16b754c
Cecios <andrea.raneri94@gmail.com>
2024-01-15 15:27:09 +0100

    2° committ

--- components.js
+++ components.js
@@ -3,7 +3,7 @@ let cart = [];
 const listaCart = document.getElementById('listaCart');
 export const  displayCards = (pics,detail,asin) => {
             return `<div class="col-6 col-md-4 col-lg-3">
-        <div class="card" style="width: 18rem;">
+        <div class="card w-100">
             <img src="${pics}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${detail}</h5>
@@ -22,16 +22,25 @@ export const  displayCards = (pics,detail,asin) => {
         </div>
     </div>`
     };
-
+// AGGIUNGO EVENTO ONCLICK
 export const addClickBtn = (obj) => {
     array = obj
     const addClickBtn = document.querySelectorAll('.cards a.btn')
     addClickBtn.forEach((element) => element.onclick = addToCart)
 };
-
+// FUNZIONE AGGIUNGI CARRELLO
  const addToCart = function (event) {
-    
-   //console.log(event);
-    const found = array.find((x)=>x.asin === event.target.id)
-    console.log(found);
+    let id = parseInt(event.target.id)
+    const check = cart.find((element)=> element === id)
+    if (check) {
+        console.log("elemento già presente",cart);
+    }
+    else{
+        const nuovoElemento = id
+        cart.push(nuovoElemento);
+        console.log(event.target.parentNode.parentNode.parentNode.classList.value);
+        console.log('elemento aggiunto',cart);
+    }
+    // const found = array.find((x)=>x.asin === event.target.id)
+    // console.log(check);
 }
